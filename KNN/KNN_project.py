bbbdd8523a0d68bdcd3db6a749a85c01e70c65af
Xinjie Duan <dxj1984@gmail.com>
2017-06-14 00:07:54 -0400

    add project

---
+++ KNN/KNN_project.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 23:33:31 2017
+
+@author: Xinjie Duan
+"""
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+#%% read data
+df = pd.read_csv('KNN_Project_Data', index_col=0)
+df.head()
+
+#%% pair plot
+#sns.pairplot(df,hue='TARGET CLASS')
+
+#%%
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(df.drop('TARGET CLASS', axis= 1 ))
+
+#%% Train Test Split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(scaled_features,df['TARGET CLASS'], test_size = 0.3)
+
+#%% Using KNN
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+
+#%% Preditions and Evaluations
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test, y_pred)) 
+print(classification_report(y_test, y_pred))
+
+#%%
+#Test different K values
+error_rate=[]
+for i in range(1,40):
+    knn = KNeighborsClassifier(n_neighbors= i)
+    knn.fit(X_train, y_train)
+    pred_i= knn.predict(X_test)
+    error_rate.append(np.mean(pred_i != y_test))
+plt.plot(range(1,40),error_rate,marker='o',markerfacecolor='red',markersize=10)
+plt.title('Error rate vs K value')
+plt.xlabel('K')
+plt.ylabel('Error rate')
+
+#%% Choose K = 30
+knn30 = KNeighborsClassifier(n_neighbors=40)
+knn30.fit(X_train,y_train)
+y_pred30 = knn30.predict(X_test)
+print(confusion_matrix(y_test, y_pred30)) 
+print(classification_report(y_test, y_pred30))
